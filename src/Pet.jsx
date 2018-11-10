133962a407ca9519251e747dbb5af95432dde156
Garnet Yang <gyang@192-168-1-3.tpgi.com.au>
2018-11-10 17:45:27 +1100

    test renaming

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Pet = ({ name, color }) => {
-  const str = 'this is my string';
+  const str =
+    'this is my string, test the maximunong of 100 chars in the same line, to see if prettier can detect this ?';
   return (
     <h2 id="my-id">
       {' '}
       {str}
-      your name: {name} - color: {color} Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas reiciendis
-      asperiores natus blanditiis itaque explicabo nam incidunt tenetur laudantium obcaecati eaque placeat, aspernatur
-      sapiente sunt tempora aut, odio expedita rem fugit eligendi illum nesciunt sint. Deserunt facere dolorem inventore
-      qui non ex consectetur eligendi quisquam? Molestias aliquid corporis atque facilis, quod adipisci. Fugit
-      assumenda, laborum, iusto nostrum ad debitis numquam sint maiores at esse, dolor eligendi modi cum obcaecati
-      facere. Quos molestiae distinctio possimus nisi quas ipsa et quasi officia dolores iure, delectus dicta est ut
-      odit quaerat enim adipisci! Necessitatibus molestiae eligendi itaque sed obcaecati animi voluptatum ipsa, tenetur
-      ut beatae perspiciatis tempore incidunt non repudiandae saepe dolore sunt est. Tempore delectus fugit quae quam
-      corrupti quaerat iure, expedita eaque facilis sint neque dolorem! Quidem, eius ratione. Deserunt, neque?{' '}
+      your name: {name} - color: {color} Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      Quas reiciendis asperiores natus blanditiis itaque explicabo nam incidunt tenetur laudantium
+      obcaecati eaque placeat, aspernatur sapiente sunt tempora aut, odio expedita rem fugit
+      eligendi illum nesciunt sint. Deserunt facere dolorem inventore qui non ex consectetur
+      eligendi quisquam? Molestias aliquid corporis atque facilis, quod adipisci. Fugit assumenda,
+      laborum, iusto nostrum ad debitis numquam sint maiores at esse, dolor eligendi modi cum
+      obcaecati facere. Quos molestiae distinctio possimus nisi quas ipsa et quasi officia dolores
+      iure, delectus dicta est ut odit quaerat enim adipisci! Necessitatibus molestiae eligendi
+      itaque sed obcaecati animi voluptatum ipsa, tenetur ut beatae perspiciatis tempore incidunt
+      non repudiandae saepe dolore sunt est. Tempore delectus fugit quae quam corrupti quaerat iure,
+      expedita eaque facilis sint neque dolorem! Quidem, eius ratione. Deserunt, neque?{' '}
     </h2>
   );
 };
